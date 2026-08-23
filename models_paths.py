32216d5b59fcac64c06981058801d00b53a950b4
Deviprasad B R L <deviprasadbrl7@gmail.com>
2026-08-23 13:02:58 +0530

    Update models_paths.py

--- models_paths.py
+++ models_paths.py
@@ -1,7 +1,7 @@
-brain_path=r"C:\Users\Devi Prasad\OneDrive\Documents\prep\Machine learning\CNN\project\model_weights\brain_expert_model.pth"
+brain_path="brain_expert_model.pth"
 
-kidney_path=r"C:\Users\Devi Prasad\OneDrive\Documents\prep\Machine learning\CNN\project\model_weights\kidney_expert_model.pth"
+kidney_path="kidney_expert_model.pth"
 
-oral_path=r"C:\Users\Devi Prasad\OneDrive\Documents\prep\Machine learning\CNN\project\model_weights\oral_expert_model.pth"
+oral_path="oral_expert_model.pth"
 
-router_path=r"C:\Users\Devi Prasad\OneDrive\Documents\prep\Machine learning\CNN\project\model_weights\router_model.pth"
+router_path="router_model.pth"
