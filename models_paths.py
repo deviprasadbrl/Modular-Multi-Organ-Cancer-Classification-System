13c6a92c469ce9d8a50c021021af276353ff6a52
Deviprasad B R L <deviprasadbrl7@gmail.com>
2026-08-23 13:02:12 +0530

    Add files via upload

---
+++ models_paths.py
@@ -0,0 +1,7 @@
+brain_path=r"C:\Users\Devi Prasad\OneDrive\Documents\prep\Machine learning\CNN\project\model_weights\brain_expert_model.pth"
+
+kidney_path=r"C:\Users\Devi Prasad\OneDrive\Documents\prep\Machine learning\CNN\project\model_weights\kidney_expert_model.pth"
+
+oral_path=r"C:\Users\Devi Prasad\OneDrive\Documents\prep\Machine learning\CNN\project\model_weights\oral_expert_model.pth"
+
+router_path=r"C:\Users\Devi Prasad\OneDrive\Documents\prep\Machine learning\CNN\project\model_weights\router_model.pth"
